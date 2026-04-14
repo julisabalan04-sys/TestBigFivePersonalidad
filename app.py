c121ae3c1a05a4d33ef43871413f05a5fe594f24
julisabalan04-sys <julisabalan04@gmail.com>
2026-04-14 07:42:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -100,6 +100,12 @@ opn_questions = {
 }
 
 lista_preguntas = [ext_questions, neu_questions,agr_questions,con_questions,opn_questions]
+lista_clusters = [
+    "El grupo 0 está compuesto por personalidades muy introvertidas, bastante nerviosas, amigables y creativas, pero ni descuidadas ni organizadas.",
+    "El grupo 1 está formado por personas muy extrovertidas, amigables, organizadas y curiosas, además de ser las más seguras de sí mismas.",
+    "El grupo 2 puede describirse como compuesto por introvertidos con un nivel medio de confianza, que son bastante amigables, organizados y bastante creativos.",
+    "El grupo 3 está formado por extrovertidos con un nivel medio de nerviosismo, que son realmente amigables y creativos, pero ni descuidados ni organizados."
+]
 
 # Initialize an empty dictionary to store user responses in session state
 if 'user_responses' not in st.session_state:
